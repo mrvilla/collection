c76ea79f2421523880673efd6ac3e2afd9b8ce64
enrique.cantillo <enrique.cantillo@aperto.com>
2019-10-02 16:37:01 +0200

    remove low level ajax request

--- src/app/shared/data/externtalData.js
+++ src/app/shared/data/externtalData.js
@@ -24,68 +24,6 @@ export function fetchApi(searchQuery) {
 		.then(res => res.json())
 		.catch(err => console.log(err));
 
-	// var data = null;
-	//
-	// var xhr = new XMLHttpRequest();
-	// xhr.withCredentials = true;
-	//
-	// xhr.addEventListener("readystatechange", function () {
-	// 	if (this.readyState === 4) {
-	// 		console.log('goodreads:', this);
-	// 	}
-	// });
-	//
-	// xhr.open("GET", `https://www.goodreads.com/search/?key=OdApwh4IJ7zOqXbUspaow&q=${searchQuery}`);
-	// xhr.setRequestHeader("cache-control", "no-cache");
-	// xhr.setRequestHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-	//
-	// xhr.send(data);
-
-	// const fetch4 = fetch(`https://comicvine.gamespot.com/api/volumes/?api_key=9ef4ea3b99257e15d1e53292bc300453c14f2712&format=json&filter=name:${searchQuery}`, {
-	// 	//method: 'GET', // *GET, POST, PUT, DELETE, etc.
-	// 	mode: 'no-cors', // no-cors, cors, *same-origin
-	// 	cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-	// 	//credentials: 'include', // include, same-origin, *omit
-	// 	headers: {
-	// 		'Content-Type': 'application/json, text/json, text/*+json, application/xml, text/xml, text/html',
-	// 	 	'Content-Type': 'application/x-www-form-urlencoded',
-	// 		'Access-Control-Allow-Credentials' : true,
-	// 	    'Access-Control-Allow-Origin' : '*', // *, http://localhost:3000
-	// 		'Access-Control-Allow-Headers': 'Content-Type, Accept, X-Requested-With',
-	// 		'Access-Control-Allow-Methods': 'GET',
-	// 		'X-Content-Type-Options': 'nosniff',
-	// 		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
-	// 	},
-	// 	redirect: "follow", // manual, *follow, error
-	// 	referrer: "no-referrer", // no-referrer, *client
-	// })
-	// 	.then((res) => res.json());
-
-	// var data = null;
-	//
-	// var xhr = new XMLHttpRequest();
-	// xhr.withCredentials = true;
-	//
-	// xhr.addEventListener("readystatechange", function () {
-	// 	if (this.readyState === 4) {
-	// 		console.log("text:",  this);
-	// 	}
-	// });
-	//
-	// xhr.open("GET", `https://comicvine.gamespot.com/api/volumes/?api_key=9ef4ea3b99257e15d1e53292bc300453c14f2712&format=json&filter=name:${searchQuery}`);
-	// xhr.setRequestHeader("cache-control", "no-cache");
-	// xhr.setRequestHeader("Postman-Token", "d8581b7d-e542-479f-addc-fd7320939d27");
-	//
-	// xhr.send(data);
-
-	function status(response) {
-		if (response.status >= 200 && response.status < 300) {
-			return Promise.resolve(response);
-		} else {
-			return Promise.reject(new Error(response.statusText));
-		}
-	}
-
 	return Promise.all([fetch1, fetch2, fetch3, fetch4])
 		.then(data => data)
 		.catch(err => console.log(err));
